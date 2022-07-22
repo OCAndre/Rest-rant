cc4907756c0695b3bb10658ebdca79ecacae8365
Owen Andre <owencandre@gmail.com>
2022-07-21 20:11:50 -0400

    Added boostrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -7,13 +7,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/Restraunt1.jpg'
     }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/Restraunt2.jpg'
     }]
 
     res.render('places/index', { places })
@@ -21,3 +21,8 @@ router.get('/', (req, res) => {
 
 
 module.exports = router
+
+// Photo references in
+// {/* Photo by <a href="https://unsplash.com/@ninjason?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Leung</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
+// Photo by <a href="https://unsplash.com/es/@digndoug?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Doug Greenman</a> on <a href="https://unsplash.com/s/photos/coffee-house?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+// {/* Photo by <a href="https://unsplash.com/@chrisliverani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Liverani</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,20 +4,32 @@ const Def = require('.././default')
 function index(data) {
     let placesFormatted = data.places.map((place) => {
         return (
-            <div>
+            <div className="col-sm-6">
                 <h2>{place.name}</h2>
+                <p className="text-center">
+                    {place.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name} />
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
+
     return (
         <Def>
             <main>
-                <h1>PLACES INDEX PAGE</h1>
-                {placesFormatted}
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                    {placesFormatted}
+                </div>
             </main>
         </Def>
     )
 }
 
 module.exports = index
+
+{/* Photo by <a href="https://unsplash.com/@ninjason?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Leung</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
+{/* Photo by <a href="https://unsplash.com/@chrisliverani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Liverani</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
