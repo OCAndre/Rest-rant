8c1e8cb8beb9788f910dd8ceaa2e8cc107610a95
Owen Andre <owencandre@gmail.com>
2022-07-22 21:16:40 -0400

    added and rendered a new view

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -30,6 +30,3 @@ function index(data) {
 }
 
 module.exports = index
-
-{/* Photo by <a href="https://unsplash.com/@ninjason?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Leung</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
-{/* Photo by <a href="https://unsplash.com/@chrisliverani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Liverani</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
