e089a3dc8bfe0a4efcf8fe45b95b220994f2afbc
Owen Andre <owencandre@gmail.com>
2022-07-28 20:53:06 -0400

    fixed show page

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../models')
 
+// Home
 router.get('/', (req, res) => {
     db.Place.find()
         .then((places) => {
@@ -12,8 +13,12 @@ router.get('/', (req, res) => {
         })
 })
 
-
+// post
 router.post('/', (req, res) => {
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
     db.Place.create(req.body)
         .then(() => {
             res.redirect('/places')
@@ -25,23 +30,34 @@ router.post('/', (req, res) => {
 })
 
 
-
+// new
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+// show
 router.get('/:id', (req, res) => {
-    res.send('GET /places/:id stub')
+    db.Place.findById(req.params.id)
+        .then(place => {
+            res.render('places/show', { place })
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
+// put
 router.put('/:id', (req, res) => {
     res.send('PUT /places/:id stub')
 })
 
+// delete
 router.delete('/:id', (req, res) => {
     res.send('DELETE /places/:id stub')
 })
 
+// edit
 router.get('/:id/edit', (req, res) => {
     res.send('GET edit form stub')
 })
