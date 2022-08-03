4a65622ffd3be64ec827419a90758426aa08c607
Owen Andre <owencandre@gmail.com>
2022-08-02 21:14:28 -0400

    added comments

--- controllers/places.js
+++ controllers/places.js
@@ -44,7 +44,9 @@ router.get('/new', (req, res) => {
 // show
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+        .populate('comments')
         .then(place => {
+            console.log(place.comments)
             res.render('places/show', { place })
         })
         .catch(err => {
@@ -60,12 +62,38 @@ router.put('/:id', (req, res) => {
 
 // delete
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
+    // res.send('DELETE /places/:id stub')
+    db.Place.findByIdAndDelete(req.params.id)
+        .then(deletedPlace => {
+            res.status(303).redirect('/places')
+        })
 })
+
 
 // edit
 router.get('/:id/edit', (req, res) => {
     res.send('GET edit form stub')
+})
+
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
 })
 
 router.post('/:id/rant', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <stong>- {c.author}</stong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -24,27 +43,49 @@ function show(data) {
                         <h4>
                             Serving {data.place.cuisines}
                         </h4>
+                        <div className="row">
+                            <div className="col-sm-6"><a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                                Edit
+                            </a></div>
+                            <div className="col-sm-6">
+                                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                                    <button type="submit" className="btn btn-danger">
+                                        Delete
+                                    </button>
+                                </form>
+                            </div></div>
                     </div></div>
                 <hr />
                 <h2>comments</h2>
-                <p className="text-center">currenty no comments</p>
+                {comments}
+                <h2>Got Your Own Rant or Rave?</h2>
+                <form action={`/places/${data.place.id}/comment`} method="POST">
+                    <label htmlFor="content">Content</label>
+                    <textarea className="form-control" id="content" name="content" />
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="author">Author</label>
+                            <input className="form-control" id="author" name="author" />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="starts">Star Rating</label>
+                            <input type="range" step="0.5" min="1" max="5" className="form-control" id="stars" name="stars" />
+                        </div>
+                        {/* checkbox will not go onto the same row as above so made a seperate line */}
+                        <br />
+
+                        <label htmlFor="rant">Rant? </label>
+                        <input type="checkbox" value="rant" id="rant" />
+
+                    </div>
+                    <br />
+                    <input type="submit" className="btn btn-primary" value="Add Comment" />
+                </form>
             </main>
-            <div className='row'>
-                <div className='col-sm-6'>
-                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                        Edit
-                    </a>
-                </div>
-                <div className='col-sm-6'>
-                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                        <button type="submit" className="btn btn-danger">
-                            Delete
-                        </button>
-                    </form>
-                </div>
-            </div>
         </Def>
     )
 }
 
+
 module.exports = show
+
